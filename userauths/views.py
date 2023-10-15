0f6b54e146d638663a8a3b82d38291c5519bf320
edwinaringo <tedaringo628@gmail.com>
2023-10-15 20:57:00 +0300

    Implemented signing out

--- userauths/views.py
+++ userauths/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from userauths.forms import UserRegisterForm
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.conf import settings
 
@@ -55,3 +55,8 @@ def login_view(request):
     }
             
     return render(request, "userauths/sign-in.html", context)
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "You have successfully logged out")
+    return redirect("userauths:sign-in")
