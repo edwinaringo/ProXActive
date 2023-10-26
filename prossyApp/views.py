c2760219564eb109c1ffbd76909a4f63b77443f8
edwinaringo <tedaringo628@gmail.com>
2023-10-26 13:19:57 +0300

    Edited the list view to get data fromthe database

--- prossyApp/views.py
+++ prossyApp/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from prossyApp.models import Product, Category, CartOrder, CartOrderItems, ProductImages, ProductReview, Address, Wishlist
+
+
 def index(request):
-    return render(request, 'core/index.html')
+    # products = Product.objects.all()
+    
+    products = Product.objects.filter(product_status="published", featured=True)
+    
+    context = {
+        "products": products
+    }
+    
+    return render(request, 'core/index.html', context)
