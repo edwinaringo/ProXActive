4c74fcd951492b4c38a08d5b47b4cff9f706537f
edwinaringo <tedaringo628@gmail.com>
2023-11-23 13:03:15 +0300

    Migrations

--- prossy/settings.py
+++ prossy/settings.py
@@ -68,7 +68,7 @@ INSTALLED_APPS = [
     
     #Custom Apps
     'prossyApp',
-    'userauths'
+    'userauths',
 ]
 
 MIDDLEWARE = [
@@ -79,7 +79,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'log_request_id.middleware.RequestIDMiddleware',
 
 ]
 
@@ -212,40 +211,6 @@ CKEDITOR_CONFIGS = {
 PAYPAL_RECEIVER_EMAIL = 'sb-pvosy28100602@business.example.com'
 PAYPAL_TEST = True
 
-# Support for X-Request-ID
-
-LOG_REQUEST_ID_HEADER = 'HTTP_X_REQUEST_ID'
-LOG_REQUESTS = True
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'request_id': {
-            '()': 'log_request_id.filters.RequestIDFilter'
-        }
-    },
-    'formatters': {
-        'standard': {
-            'format': '%(levelname)-8s [%(asctime)s] [%(request_id)s] %(name)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'filters': ['request_id'],
-            'formatter': 'standard',
-        },
-    },
-    'loggers': {
-        'log_request_id.middleware': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    }
-}
 
 
 
